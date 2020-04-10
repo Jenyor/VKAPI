2458a989f64a8a0947a62cfe8d2641feef63676e
mAlc_Jke <malc.jke@yandex.ru>
2020-04-10 21:55:48 +0300

    fix
    Excess removed

--- examples/users_chat.php
+++ examples/users_chat.php
@@ -1,12 +1,11 @@
 <?php
-$VK = new VKAPI("xxxxxxx", "5.103");
+$VK = new VKAPI("xxxxxxx", "5.103"); // 'xxxxxxx' - token vk, '5.103' - version vkapi
 
 $users = $VK->messages_getChat('182', null, ['status'])->response->users;
 
-$final = array();
 for ($i=0; $i < count($users); $i++) {
-  if(!isset($users[$i]->deactivated))
-    echo "{$users[$i]->first_name} {$users[$i]->last_name}\n"; // Проверка на удаленных и забаенных пользователей
+  if(!isset($users[$i]->deactivated)) // Проверка на удаленных и забаенных пользователей
+    echo "{$users[$i]->first_name} {$users[$i]->last_name}\n";  // Вывод имени и фамилии
 }
 
  ?>
